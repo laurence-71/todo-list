e226d4ed206fdfcf73e587b019b99cf5d0ebd50f
Laurence <laurencedupupet84@gmail.com>
2020-12-07 12:17:03 +0100

    change on env

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  jsonbin:{
-    key:"$2b$10$CIla.uBMsWWb5MHbGa4il.AunyCXFYqI75DOgoKZ.pndSP0p1YnTO",
-    url:"https://api.jsonbin.io/b/5fbe5ece90e7c66167f6ce81"
-  }
-};
